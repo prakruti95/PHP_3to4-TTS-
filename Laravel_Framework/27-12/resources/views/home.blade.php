40a838b888610d05252f5f6ecff66b5ee7a4433e
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-12-27 16:18:34 +0530

    commit

---
+++ Laravel_Framework/27-12/resources/views/home.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ route('students.list') }}">View Students</a>
---
+++ Laravel_Framework/27-12/resources/views/students.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Students</title>
+</head>
+<body>
+    <h1>Student List</h1>
+    <ul>
+        <li>Rahul</li>
+        <li>Anita</li>
+        <li>Arjun</li>
+    </ul>
+</body>
+</html>
